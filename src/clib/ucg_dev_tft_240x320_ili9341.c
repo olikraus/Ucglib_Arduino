446d37b484284358b5572253bcf580c07846fee8
olikraus <olikraus@gmail.com>
2015-05-25 23:40:15 +0200

    v1.02pre1

--- src/clib/ucg_dev_tft_240x320_ili9341.c
+++ src/clib/ucg_dev_tft_240x320_ili9341.c
@@ -102,6 +102,7 @@ static const ucg_pgm_uint8_t ucg_tft_240x320_ili9341_init_seq[] = {
   UCG_END(),					/* end of sequence */
 };
 
+
 ucg_int_t ucg_dev_ili9341_18x240x320(ucg_t *ucg, ucg_int_t msg, void *data)
 {
   switch(msg)
